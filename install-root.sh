71b69aea065e97052b08c38c4b9b80b893cb2837
Bartłomiej Kamiński <fizyk20@gmail.com>
2018-08-22 22:17:26 +0200

    Make nvidia card always powered

--- install-root.sh
+++ install-root.sh
@@ -8,22 +8,11 @@ MAINDIR=$(pwd)
 
 cp $MAINDIR/lightdm/* /etc/lightdm/
 
-# xorg.conf unnecessary and might be left over from other experiments
+# disable bumblebee
 
-if [ -f "/etc/X11/xorg.conf" ]; then
-	rm /etc/X11/xorg.conf
-fi
+systemctl disable bumblebeed.service
 
-# enable bumblebee
+rm /etc/modprobe.d/bumblebee.conf
+rm /etc/modprobe.d/bbswitch.conf
 
-systemctl enable bumblebeed.service
-
-echo "blacklist nvidia" > /etc/modprobe.d/bumblebee.conf
-echo "blacklist nvidia-drm" >> /etc/modprobe.d/bumblebee.conf
-echo "blacklist nvidia-modeset" >> /etc/modprobe.d/bumblebee.conf
-echo "blacklist nvidia-uvm" >> /etc/modprobe.d/bumblebee.conf
-echo "blacklist nouveau" >> /etc/modprobe.d/bumblebee.conf
-
-# configure bbswitch
-
-echo "options bbswitch load_state=0 unload_state=1" > /etc/modprobe.d/bbswitch.conf
+cp $MAINDIR/xorg.conf /etc/X11/xorg.conf
