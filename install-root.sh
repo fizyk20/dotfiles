bcfde7d6f8810bd7258b2359ac294ae46f78d7ea
Bartłomiej Kamiński <fizyk20@gmail.com>
2018-08-22 22:12:49 +0200

    Automate recreation of graphics config

--- install-root.sh
+++ install-root.sh
@@ -7,3 +7,23 @@ fi
 MAINDIR=$(pwd)
 
 cp $MAINDIR/lightdm/* /etc/lightdm/
+
+# xorg.conf unnecessary and might be left over from other experiments
+
+if [ -f "/etc/X11/xorg.conf" ]; then
+	rm /etc/X11/xorg.conf
+fi
+
+# enable bumblebee
+
+systemctl enable bumblebeed.service
+
+echo "blacklist nvidia" > /etc/modprobe.d/bumblebee.conf
+echo "blacklist nvidia-drm" >> /etc/modprobe.d/bumblebee.conf
+echo "blacklist nvidia-modeset" >> /etc/modprobe.d/bumblebee.conf
+echo "blacklist nvidia-uvm" >> /etc/modprobe.d/bumblebee.conf
+echo "blacklist nouveau" >> /etc/modprobe.d/bumblebee.conf
+
+# configure bbswitch
+
+echo "options bbswitch load_state=0 unload_state=1" > /etc/modprobe.d/bbswitch.conf
